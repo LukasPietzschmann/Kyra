d89f0e043c5a09c53eb055c1c33afd770918b8e5
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-08-11 18:27:50 +0200

    A tokens value can now be retreived as an int
    Previousy the value of a token was stored in a std::string.
    This is still the case, but the string gets wrapped in a struct which
    allows for convenient conversion to an int.
    More types might be added in the future.

--- src/LibSlg/Token.hpp
+++ src/LibSlg/Token.hpp
@@ -8,25 +8,31 @@
 namespace LibSlg {
 class Token {
 public:
+	typedef struct TokenValue {
+		explicit TokenValue(std::string val) : value(std::move(val)) {};
+		std::string value;
+		int asInt() const { return std::stoi(value); };
+		std::string asString() const { return value; };
+	} TokenValue;
 	Token(const TokenType& type, int line, std::string lexeme, std::string value = "") :
-		m_type(type), m_line(line), m_lexeme(std::move(lexeme)), m_value(std::move(value)) {}
+			m_type(type), m_line(line), m_lexeme(std::move(lexeme)), m_value(TokenValue(std::move(value))) {}
 	TokenType getType() const { return m_type; }
 	int getLine() const { return m_line; }
 	std::string getLexeme() const { return m_lexeme; }
-	std::string getValue() const { return m_value; }
+	TokenValue getValue() const { return m_value; }
 
 	friend std::ostream& operator<<(std::ostream& os, const Token& token) {
 		os << "Type " << TokenTypeName::getFor(token.m_type) << " at line " << token.m_line;
 		if(!token.m_lexeme.empty())
 			os << " with lexeme " << token.m_lexeme;
-		if(!token.m_value.empty())
-			os << " and a value of " << token.m_value;
+		if(!token.m_value.value.empty())
+			os << " and a value of " << token.m_value.value;
 		return os;
 	}
 private:
 	const TokenType m_type;
 	const int m_line;
 	const std::string m_lexeme;
-	const std::string m_value;
+	const TokenValue m_value;
 };
 }
