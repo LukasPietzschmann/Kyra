7667b719cc9513abc2bd26a22a3adf1427865056
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-08-13 12:37:47 +0200

    Added a forward header for all expressions

---
+++ src/LibSlg/Expressions/Forward.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace LibSlg {
+class AssignmentExpr;
+class BinaryExpr;
+class CallExpr;
+class FunctionExpr;
+class LiteralExpr;
+class ObjectExpr;
+class UnaryExpr;
+class VariableExpr;
+}
