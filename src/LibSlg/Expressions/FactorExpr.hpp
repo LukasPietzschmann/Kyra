5ca0182673204c7a3ced9a1c4d8e81d10a667d07
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-08-13 12:37:22 +0200

    Removed unnecessary expr classes

--- src/LibSlg/Expressions/FactorExpr.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Expression.hpp"
-
-namespace LibSlg {
-class FactorExpr : public Expression {};
-}
--- src/LibSlg/Expressions/PrimaryExpr.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Expression.hpp"
-
-namespace LibSlg {
-class PrimaryExpr : public Expression {};
-}
