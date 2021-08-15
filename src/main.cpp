81d3730a6564a2ffa192f4ace2a2ccfe56bb15d7
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-08-15 21:37:47 +0200

    Added a simple REPL

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
+#include <fstream>
 #include "LibSlg/Lexer.hpp"
+#include "LibSlg/Parser.hpp"
+#include "LibSlg/AstLogger.hpp"
 
-int main() {
-	LibSlg::Lexer lexer("(){},;.-+/*	= == != !> >= < <=name  \"string\" 420 var fun object print nothing true false return");
-	for(const auto &token : lexer.scanTokens())
-		std::cout << token << std::endl;
+int main(int argc, char** argv) {
+	if(argc < 2) {
+		const std::string prompt = "SLG > ";
+		// REPL
+		std::cout << prompt;
+		for(std::string line; std::getline(std::cin, line);) {
+			LibSlg::Lexer lexer(line);
+
+			auto tokens = lexer.scanTokens();
+			for(const auto& token : tokens)
+				std::cout << token << std::endl;
+
+			LibSlg::Parser parser(tokens);
+			for(const auto& statement : parser.parse()) {
+				LibSlg::AstLogger logger;
+				logger.logStatement(statement);
+			}
+
+			std::cout << prompt;
+		}
+	}else {
+		// File
+		std::stringstream fileContent;
+		std::string line;
+		std::ifstream fileStream(*(argv + 1));
+		while(std::getline(fileStream, line))
+			fileContent << line << "\n";
+		fileStream.close();
+
+		LibSlg::Lexer lexer(fileContent.str());
+		auto tokens = lexer.scanTokens();
+		for(const auto& token : tokens)
+			std::cout << token << std::endl;
+
+		LibSlg::Parser parser(tokens);
+		for(const auto& statement : parser.parse()) {
+			LibSlg::AstLogger logger;
+			logger.logStatement(statement);
+		}
+	}
 
 	return 0;
 }
