51f6850785e95add17d3862bf3f27f221e32d654
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-08-16 09:06:38 +0200

    Added constructor to AssignmentExpr

--- src/LibSlg/Expressions/AssignmentExpr.hpp
+++ src/LibSlg/Expressions/AssignmentExpr.hpp
@@ -6,6 +6,7 @@
 namespace LibSlg {
 class AssignmentExpr : public Expression {
 public:
+	AssignmentExpr(const Token& name, Expression::Ptr  newValue) : m_name(name), m_newValue(std::move(newValue)) {}
 	void accept(ExpressionVisitor& visitor) override { visitor.visitAssignmentExpr(*this); }
 	const Token& getName() const { return m_name; }
 	const Expression::Ptr& getNewValue() const { return m_newValue; }
