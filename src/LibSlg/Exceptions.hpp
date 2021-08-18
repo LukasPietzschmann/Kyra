1de50b74c77bae53659502b69adfed6fa2c90d24
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-08-18 19:59:02 +0200

    Added a RuntimeException class

--- src/LibSlg/Exceptions.hpp
+++ src/LibSlg/Exceptions.hpp
@@ -11,4 +11,12 @@ public:
 private:
 	std::string m_message;
 };
+
+class RuntimeException : std::exception {
+public:
+	explicit RuntimeException(std::string message) : m_message(std::move(message)) {}
+	const char* what() const noexcept override { return m_message.c_str(); }
+private:
+	std::string m_message;
+};
 }
