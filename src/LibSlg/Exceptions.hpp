9fe1495e29b4efb70dfc36c350cb44b0411e5cda
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-08-30 15:20:32 +0200

    Cleaned up Exceptions

--- src/LibSlg/Exceptions.hpp
+++ src/LibSlg/Exceptions.hpp
@@ -4,31 +4,31 @@
 #include <utility>
 
 namespace LibSlg {
-class LexerException : std::exception {
+class MessageException : std::exception {
 public:
-	explicit LexerException(std::string message) : m_message(std::move(message)) {}
-	const char* what() const noexcept override { return m_message.c_str(); }
-private:
+	explicit MessageException(const std::string& message) : m_message(message) {}
+	virtual const char* what() const noexcept override { return m_message.c_str(); }
+protected:
 	std::string m_message;
 };
 
-class ParserException : std::exception {
+class LexerException : public MessageException {
+public:
+	explicit LexerException(std::string message) : MessageException(message) {}
+};
+
+class ParserException : public MessageException {
 public:
 	explicit ParserException(std::string message, bool unfinished = false) :
-			m_message(std::move(message)), m_unfinished(unfinished) {}
-	const char* what() const noexcept override { return m_message.c_str(); }
+			MessageException(message), m_unfinished(unfinished) {}
 	bool isUnfinished() const { return m_unfinished; }
 private:
-	std::string m_message;
 	const bool m_unfinished;
 };
 
-class RuntimeException : std::exception {
+class RuntimeException : public MessageException {
 public:
-	explicit RuntimeException(std::string message) : m_message(std::move(message)) {}
-	const char* what() const noexcept override { return m_message.c_str(); }
-private:
-	std::string m_message;
+	explicit RuntimeException(std::string message) : MessageException(message) {}
 };
 
 class Value;
