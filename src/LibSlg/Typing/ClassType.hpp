54fcc4b3e604f63fbfe2e8b8c0500380ea653e1c
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-09-13 16:58:35 +0200

    Class-types now store their constructor parameter types

--- src/LibSlg/Typing/ClassType.hpp
+++ src/LibSlg/Typing/ClassType.hpp
@@ -3,14 +3,17 @@
 #include <string>
 #include <unordered_map>
 #include <utility>
+#include <vector>
 
 #include "Type.hpp"
 
 namespace LibSlg {
 class ClassType : public Type {
 public:
-	ClassType(const std::string& name, bool isMutable, std::unordered_map<std::string, Type::Ptr> declarations = {}) :
-		Type(name, isMutable), m_declarations(std::move(declarations)) {}
+	ClassType(const std::string& name, bool isMutable, std::unordered_map<std::string, Type::Ptr> declarations = {},
+			std::vector<Type::Ptr> constructorParams = {}) :
+		Type(name, isMutable),
+		m_declarations(std::move(declarations)), m_constructorParameter(std::move(constructorParams)) {}
 	~ClassType() override = default;
 	Type::Ptr knowsAbout(const std::string& name) const override {
 		auto it = m_declarations.find(name);
@@ -21,7 +24,11 @@ public:
 
 	Type::Ptr duplicate() const override { return Type::makePtr<ClassType>(*this); }
 
+	const std::vector<Type::Ptr>& getConstructorParameter() const { return m_constructorParameter; }
+	unsigned int getArity() const { return m_constructorParameter.size(); }
+
 protected:
 	std::unordered_map<std::string, Type::Ptr> m_declarations;
+	std::vector<Type::Ptr> m_constructorParameter;
 };
 }
