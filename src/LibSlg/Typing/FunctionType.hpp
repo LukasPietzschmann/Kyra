5eb5662c2976284d90da685590b9e0e215c59636
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-09-16 13:56:52 +0200

    FunctionTypes are now compared based on their param and return types

--- src/LibSlg/Typing/FunctionType.hpp
+++ src/LibSlg/Typing/FunctionType.hpp
@@ -7,12 +7,29 @@
 namespace LibSlg {
 class FunctionType : public Type {
 public:
-	FunctionType(bool isMutable, Ptr returnType, std::vector<Type::Ptr> parameters) :
+	FunctionType(bool isMutable, Type::Ptr returnType, std::vector<Type::Ptr> parameters) :
 		Type("Function", isMutable), m_returnType(std::move(returnType)), m_parameters(std::move(parameters)) {}
 	~FunctionType() override = default;
 	const Type::Ptr& getReturnType() const { return m_returnType; }
 	std::vector<Type::Ptr> getParameters() const { return m_parameters; }
 	Type::Ptr duplicate() const override { return Type::makePtr<FunctionType>(*this); }
+
+	bool operator==(const Type::Ptr& other) const override {
+		if(other->getName() != "Function")
+			return false;
+		const auto& castedOther = std::dynamic_pointer_cast<FunctionType>(other);
+		bool areParamsEqual = true;
+		if(m_parameters.size() != castedOther->getParameters().size())
+			areParamsEqual = false;
+		for(unsigned long i = 0; i < m_parameters.size(); ++i) {
+			if(*m_parameters[i] == castedOther->m_parameters[i])
+				continue;
+			areParamsEqual = false;
+			break;
+		}
+
+		return *m_returnType == castedOther->m_returnType && areParamsEqual;
+	}
 
 protected:
 	Type::Ptr m_returnType;
