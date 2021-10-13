5e4f064148237863493dec0d6c9a944bbdc0f0a1
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-10-13 22:26:33 +0200

    Added a dedicated Runtime. This will replace the Interpreter

---
+++ src/LibSlg/Runtime/Runtime.cpp
@@ -0,0 +1,144 @@
+#include "Runtime.hpp"
+
+namespace LibSlg {
+Runtime& Runtime::getInstance() {
+	static Runtime instance;
+	return instance;
+}
+
+void Runtime::executeStatement(const Statement::Ptr& statement) { statement->accept(*this); }
+
+Value::Ptr Runtime::executeExpression(const Expression::Ptr& expression) {
+	EXPR_ACCEPT(expression, *this, Value::Ptr result);
+	return result;
+}
+
+void Runtime::visitAccessExpr(AccessExpr& accessExpr) {
+	EXPR_ACCEPT(accessExpr.getOwner(), *this, Value::Ptr owner);
+	const auto& klass = Value::as<Klass>(owner);
+	EXPR_RETURN_FROM_VISIT(klass->getInstanceContext()->getVar(accessExpr.getName().getValue().asString()).value);
+}
+
+void Runtime::visitAssignmentExpr(AssignmentExpr& assignmentExpr) {
+	EXPR_ACCEPT(assignmentExpr.getNewValue(), *this, Value::Ptr newValue);
+	m_currentContext.mutate(assignmentExpr.getName().getValue().asString(), newValue);
+	EXPR_RETURN_FROM_VISIT(newValue);
+}
+
+void Runtime::visitBinaryExpr(BinaryExpr& binaryExpr) {
+	EXPR_ACCEPT(binaryExpr.getLhs(), *this, Value::Ptr lhs);
+	EXPR_ACCEPT(binaryExpr.getRhs(), *this, Value::Ptr rhs);
+	switch(binaryExpr.getOperator().getType()) {
+		case TokenType::EQUAL_EQUAL: EXPR_RETURN_FROM_VISIT(Value::makePtr<Bool>(*lhs == rhs));
+		case TokenType::BANG_EQUAL: EXPR_RETURN_FROM_VISIT(Value::makePtr<Bool>(*lhs != rhs));
+		case TokenType::GREATER: EXPR_RETURN_FROM_VISIT(Value::makePtr<Bool>(*lhs > rhs));
+		case TokenType::GREATER_EQUAL: EXPR_RETURN_FROM_VISIT(Value::makePtr<Bool>(*lhs >= rhs));
+		case TokenType::LESS: EXPR_RETURN_FROM_VISIT(Value::makePtr<Bool>(*lhs < rhs));
+		case TokenType::LESS_EQUAL: EXPR_RETURN_FROM_VISIT(Value::makePtr<Bool>(*lhs <= rhs));
+		case TokenType::MINUS: EXPR_RETURN_FROM_VISIT(*lhs - rhs);
+		case TokenType::PLUS: EXPR_RETURN_FROM_VISIT(*lhs + rhs);
+		case TokenType::STAR: EXPR_RETURN_FROM_VISIT(*lhs * rhs);
+		case TokenType::SLASH: EXPR_RETURN_FROM_VISIT(*lhs / rhs);
+		default: assert(false);
+	}
+}
+
+void Runtime::visitCallExpr(CallExpr& callExpr) {
+	EXPR_ACCEPT(callExpr.getFunction(), *this, Value::Ptr gFunction);
+	auto function = Value::as<Function>(gFunction);
+	assert(callExpr.getArguments().size() == function->getArity());
+
+	std::vector<Value::Ptr> args;
+	for(const auto& arg : callExpr.getArguments()) {
+		EXPR_ACCEPT(arg, *this, Value::Ptr value);
+		args.push_back(value);
+	}
+	Value::Ptr returnVal = function->exec(args);
+	EXPR_RETURN_FROM_VISIT(returnVal);
+}
+
+void Runtime::visitFunction(FunctionExpr& functionExpr) {
+	EXPR_RETURN_FROM_VISIT(Value::makePtr<Function>(functionExpr, m_currentContext));
+}
+
+void Runtime::visitGroupExpr(GroupExpr& groupExpr) {
+	EXPR_ACCEPT(groupExpr.getExpr(), *this, Value::Ptr value);
+	EXPR_RETURN_FROM_VISIT(value);
+}
+
+void Runtime::visitInstantiationExpr(InstantiationExpr& instantiationExpr) {
+	Value::Ptr klass = Value::makePtr<Klass>(m_currentContext.getCustomType(instantiationExpr.getName()));
+	std::vector<Value::Ptr> args;
+	for(const auto& arg : instantiationExpr.getArguments()) {
+		EXPR_ACCEPT(arg, *this, Value::Ptr value);
+		args.emplace_back(value);
+	}
+	Value::as<Klass>(klass)->instantiate(args);
+	EXPR_RETURN_FROM_VISIT(klass);
+}
+
+void Runtime::visitLiteral(LiteralExpr& literalExpr) { EXPR_RETURN_FROM_VISIT(literalExpr.getValue()); }
+
+void Runtime::visitTypeExpr(TypeExpr&) {
+	// a type expr. has no effect during runtime
+}
+
+void Runtime::visitUnaryExpr(UnaryExpr& unaryExpr) {
+	EXPR_ACCEPT(unaryExpr.getRhs(), *this, Value::Ptr value);
+	switch(unaryExpr.getOperator().getType()) {
+		case TokenType::BANG: EXPR_RETURN_FROM_VISIT(Value::makePtr<Bool>(!value->isImplicitlyTrue()));
+		case TokenType::MINUS: EXPR_RETURN_FROM_VISIT(*value * Value::makePtr<Number>(-1));
+		default: assert(false);
+	}
+}
+
+void Runtime::visitVariable(VariableExpr& variableExpr) {
+	EXPR_RETURN_FROM_VISIT(m_currentContext.getVar(variableExpr.getName().getValue().asString()).value);
+}
+
+void Runtime::visitBlockStmt(BlockStmt& blockStmt) {
+	runInNewContext(
+			[&blockStmt, this]() {
+				for(const auto& statement : blockStmt.getStatements())
+					statement->accept(*this);
+			},
+			&m_currentContext);
+}
+
+void Runtime::visitDeclarationStmt(DeclarationStmt& declarationStmt) {
+	Value::Ptr init = Value::makePtr<Nothing>();
+	if(declarationStmt.getInitializer() != nullptr) {
+		EXPR_ACCEPT(declarationStmt.getInitializer(), *this, init);
+	}
+
+	const std::string& name = declarationStmt.getIdentifier().getValue().asString();
+	m_currentContext.declareVar(name, init, declarationStmt.isMutable());
+}
+
+void Runtime::visitClassDeclarationStmt(ClassDeclarationStmt& classDeclarationStmt) {
+	const std::string& name = classDeclarationStmt.getIdentifier().getValue().asString();
+	m_currentContext.declareType(name, Klass(classDeclarationStmt));
+}
+
+void Runtime::visitExpressionStmt(ExpressionStmt& expressionStmt) { expressionStmt.getExpr()->accept(*this); }
+
+void Runtime::visitPrintStmt(PrintStmt& printStmt) {
+	EXPR_ACCEPT(printStmt.getExpr(), *this, Value::Ptr value);
+	std::cout << value->toString() << std::endl;
+}
+
+void Runtime::visitReturnStmt(ReturnStmt& returnStmt) {
+	EXPR_ACCEPT(returnStmt.getExpr(), *this, Value::Ptr value);
+	throw ReturnException(value);
+}
+
+template <typename Callback>
+RuntimeContext Runtime::runInNewContext(const Callback& callback, RuntimeContext* parent) {
+	RuntimeContext currentContextCopy = std::move(m_currentContext);
+	m_currentContext = RuntimeContext(parent);
+	callback();
+	RuntimeContext modifiedContext = std::move(m_currentContext);
+	m_currentContext = std::move(currentContextCopy);
+	return modifiedContext;
+}
+}
---
+++ src/LibSlg/Runtime/Runtime.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "../Exceptions.hpp"
+#include "../Expressions/AccessExpr.hpp"
+#include "../Expressions/AssignmentExpr.hpp"
+#include "../Expressions/BinaryExpr.hpp"
+#include "../Expressions/CallExpr.hpp"
+#include "../Expressions/Expression.hpp"
+#include "../Expressions/FunctionExpr.hpp"
+#include "../Expressions/GroupExpr.hpp"
+#include "../Expressions/InstantiationExpr.hpp"
+#include "../Expressions/LiteralExpr.hpp"
+#include "../Expressions/TypeExpr.hpp"
+#include "../Expressions/UnaryExpr.hpp"
+#include "../Expressions/VariableExpr.hpp"
+#include "../Statements/BlockStmt.hpp"
+#include "../Statements/ClassDeclarationStmt.hpp"
+#include "../Statements/DeclarationStmt.hpp"
+#include "../Statements/ExpressionStmt.hpp"
+#include "../Statements/PrintStmt.hpp"
+#include "../Statements/ReturnStmt.hpp"
+#include "../Statements/Statement.hpp"
+#include "../Values/Bool.hpp"
+#include "../Values/Function.hpp"
+#include "../Values/Klass.hpp"
+#include "../Values/Number.hpp"
+#include "RuntimeContext.hpp"
+
+namespace LibSlg {
+class Runtime : public ExpressionVisitor, public StatementVisitor {
+public:
+	EXPR_NEEDS_VISIT_RETURN_OF_TYPE(Value::Ptr);
+	static Runtime& getInstance();
+
+	Runtime(Runtime const&) = delete;
+	Runtime(Runtime&&) noexcept = delete;
+	Runtime& operator=(Runtime const&) = delete;
+	Runtime& operator=(Runtime&&) = delete;
+
+	void executeStatement(const Statement::Ptr& statement);
+	Value::Ptr executeExpression(const Expression::Ptr& expression);
+
+	void visitAccessExpr(AccessExpr& accessExpr) override;
+	void visitAssignmentExpr(AssignmentExpr& assignmentExpr) override;
+	void visitBinaryExpr(BinaryExpr& binaryExpr) override;
+	void visitCallExpr(CallExpr& callExpr) override;
+	void visitFunction(FunctionExpr& functionExpr) override;
+	void visitGroupExpr(GroupExpr& groupExpr) override;
+	void visitInstantiationExpr(InstantiationExpr& instantiationExpr) override;
+	void visitLiteral(LiteralExpr& literalExpr) override;
+	void visitTypeExpr(TypeExpr& typeExpr) override;
+	void visitUnaryExpr(UnaryExpr& unaryExpr) override;
+	void visitVariable(VariableExpr& variableExpr) override;
+
+	void visitBlockStmt(BlockStmt& blockStmt) override;
+	void visitDeclarationStmt(DeclarationStmt& declarationStmt) override;
+	void visitClassDeclarationStmt(ClassDeclarationStmt& classDeclarationStmt) override;
+	void visitExpressionStmt(ExpressionStmt& expressionStmt) override;
+	void visitPrintStmt(PrintStmt& printStmt) override;
+	void visitReturnStmt(ReturnStmt& returnStmt) override;
+
+private:
+	Runtime() = default;
+
+	RuntimeContext m_currentContext{RuntimeContext()};
+
+	template <typename Callback>
+	RuntimeContext runInNewContext(const Callback& callback, RuntimeContext* parent);
+};
+}
