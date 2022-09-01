00c31eb376a3dd000a1bc190a1a6278d4203e178
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-09-01 10:48:46 +0200

    Updated error printing

--- Compiler/Error.cpp
+++ Compiler/Error.cpp
@@ -14,29 +14,41 @@ const char* ErrorException::what() const noexcept { return m_message.data(); }
 Error::Error(const ErrorException& exception) : m_exception(exception) {}
 
 void Error::print(std::ostream& stream) const {
-	stream << m_exception.what() << std::endl;
-	const SourceRange& sr = m_exception.m_source_range;
-	if(sr.get_start().line - sr.get_end().line == 0) {
+	stream << m_exception.what() << '\n';
+	const SourceRange& source_range = m_exception.m_source_range;
+	if(source_range.get_start().line - source_range.get_end().line == 0) {
 		static std::map<std::filesystem::path, std::string> file_content_cache;
 
+		const std::filesystem::path& file_path = source_range.get_file_path();
+
 		std::string file_content;
-		if(const auto& it = file_content_cache.find(sr.get_file_path()); it != file_content_cache.end())
+		if(const auto& it = file_content_cache.find(file_path); it != file_content_cache.end())
 			file_content = it->second;
 		else {
-			std::ifstream input(sr.get_file_path());
+			std::ifstream input(file_path);
 			file_content = std::string((std::istreambuf_iterator<char>(input)), (std::istreambuf_iterator<char>()));
-			file_content_cache.try_emplace(sr.get_file_path(), file_content);
+			file_content_cache.try_emplace(file_path, file_content);
+			input.close();
 		}
-		static const unsigned margin = 10;
-		const unsigned lower_index = sr.get_start().index -
-			std::min<unsigned>(std::max(margin, margin / 2), sr.get_start().index - sr.get_start().line_start_index);
-		const unsigned upper_index =
-			std::min<unsigned>(file_content.find('\n', sr.get_end().index), sr.get_end().index + margin);
-		std::string span = file_content.substr(lower_index, upper_index - lower_index);
-		stream << span << std::endl;
 
-		std::string underline(sr.get_end().index - sr.get_start().index, '-');
-		std::string padding(sr.get_start().index - lower_index, ' ');
-		stream << padding << underline << std::endl;
+		static const unsigned margin = 10;
+		const unsigned start_index = source_range.get_start().index;
+		const unsigned end_index = source_range.get_end().index;
+		const unsigned lower_index = start_index -
+			std::min(std::max(margin, margin / 2), start_index - source_range.get_start().line_start_index);
+		const unsigned upper_index = std::min<unsigned>(file_content.find('\n', end_index), end_index + margin);
+		std::string span = file_content.substr(lower_index, upper_index - lower_index);
+
+		// Replace tabs with four spaces
+		unsigned replacements = 0;
+		for(size_t n = 0; (n = span.find('\t', n)) != std::string::npos; ++replacements)
+			span.replace(n, 1, "    ");
+
+		std::string line_number = std::to_string(source_range.get_start().line) + ": ";
+		std::string underline(end_index - start_index, '~');
+		std::string padding(start_index - lower_index + line_number.length() + replacements * 3, ' ');
+		stream << line_number << span << '\n';
+		stream << padding << "\033[31m" << underline << "\033[0m\n";
 	}
+	stream.flush();
 }
