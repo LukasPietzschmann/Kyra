19e2c721bfdc24f77cca681856ec81043d836c5d
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-09-13 20:36:59 +0200

    Moved visit macros to Aliases.hpp

--- Utils/Aliases.hpp
+++ Utils/Aliases.hpp
@@ -16,3 +16,38 @@ RefPtr<T> inline mk_ref(Args... args) {
 
 template <typename T, typename... Args>
 using All = typename std::enable_if_t<std::conjunction_v<std::is_convertible<Args, T>...>>;
+
+#define VISIT_RETURN_TYPE(type)                              \
+private:                                                     \
+	type _return_value;                                      \
+	bool _has_return_value{false};                           \
+	template <typename... Args>                              \
+	auto _construct_return_type(Args... args) {              \
+		if constexpr(std::is_pointer_v<type>)                \
+			return new std::remove_pointer_t<type>(args...); \
+		else                                                 \
+			return std::remove_pointer_t<type>(args...);     \
+	}
+
+#define return_from_visit_emplace(...)                       \
+	do {                                                     \
+		_return_value = _construct_return_type(__VA_ARGS__); \
+		_has_return_value = true;                            \
+		return;                                              \
+	} while(0)
+
+#define return_from_visit(value)  \
+	do {                          \
+		_return_value = value;    \
+		_has_return_value = true; \
+		return;                   \
+	} while(0)
+
+// https://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html#Statement-Exprs
+#define visit_with_return(visitee)                                                \
+	({                                                                            \
+		_has_return_value = false;                                                \
+		(visitee).accept(*this);                                                  \
+		assert(_has_return_value && "no value was returned from previous visit"); \
+		_return_value;                                                            \
+	})
