5eb5c1d041121ac8ea02adc4845ecee744e79d1f
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-09-16 15:42:20 +0200

    Added code-gen for the print operator

--- Compiler/Plattform.hpp
+++ Compiler/Plattform.hpp
@@ -1,3 +1,5 @@
 #define BYTE_SIZE 8
 #define C_INT_BYTE_WIDTH sizeof(int)
 #define C_INT_BIT_WIDTH (C_INT_BYTE_WIDTH * BYTE_SIZE)
+#define C_CHAR_BYTE_WIDTH sizeof(char)
+#define C_CHAR_BIT_WIDTH (C_CHAR_BYTE_WIDTH * BYTE_SIZE)
